233ef2fc1f4f9119425b96d6381b0ed93b224ce0
Sebastian Lozano Herrera <sebaslh12@users.noreply.github.com>
2016-04-18 10:55:04 -0500

    Jump and collision Sound

--- Scripts/Ball_control.cs
+++ Scripts/Ball_control.cs
@@ -2,17 +2,23 @@
 
 public class Ball_control : MonoBehaviour {
 
-    public float jumpHeight;
-    public float tranSpeed;
+	private float jumpHeight = 500.0f;
+    public float tranSpeed = 50.0f;
     private bool isJumping = false;
     private int points = 0;
     Rigidbody2D body;
+	private AudioClip jumpAudio;
+	private AudioClip collideAudio;
+	private AudioSource source; 
 
 	// Use this for initialization
 	void Start () {
-
-        body = GetComponent<Rigidbody2D>();
-	    
+        body = GetComponent<Rigidbody2D>(); 
+		jumpAudio = (AudioClip)Resources.Load ("ballJump", typeof(AudioClip));
+		collideAudio = (AudioClip)Resources.Load ("ballCollide", typeof(AudioClip));
+	}
+	void Awake(){
+		source = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
@@ -20,6 +26,7 @@ public class Ball_control : MonoBehaviour {
         
         if(Input.GetKeyDown(KeyCode.Space)){			
             if (isJumping == false){
+				source.PlayOneShot (jumpAudio, 1.0f);
 				points+=5;
                 body.AddForce(Vector2.up * jumpHeight);
                 isJumping = true;                
@@ -44,8 +51,11 @@ public class Ball_control : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D col) { 
         if(col.gameObject.tag == "Platform"){
-			if(col.gameObject.transform.position.y < transform.position.y)
-            	isJumping = false;
+			source.Stop ();			
+			source.PlayOneShot (collideAudio, 1.0f);	
+			if (col.gameObject.transform.position.y < transform.position.y) {						
+				isJumping = false;			
+			}
         }
         
     }
